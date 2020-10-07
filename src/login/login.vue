fbaea7eb67d234cfa3241e4bb86c4cf00dd86bea
wangyayi <413946849@qq.com>
2020-10-07 19:10:20 +0800

    login

---
+++ src/login/login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="login-container">
+    登录组件
+  <el-button type="info">信息按钮</el-button>
+   <el-button type="danger">危险按钮</el-button>
+   <el-button type="warning">警告按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="less">
+.login-container{
+  background-color: #2b4b6b;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+</style>
