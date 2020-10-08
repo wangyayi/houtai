42b7dd368d9bd0c3709060b0036c187ccd9206a1
wangyayi <413946849@qq.com>
2020-10-08 09:01:24 +0800

    login

--- src/login/login.vue
+++ src/login/login.vue
@@ -34,8 +34,7 @@ export default {
           { min: 3, max: 75, message: '长度在 3 到 7 个字符', trigger: 'blur' }
         ],
         password: [
-          { required: true, message: '请输入密码', trigger: 'blur' },
-          {}
+          { required: true, message: '请输入密码', trigger: 'blur' }
         ]
       }
     }
